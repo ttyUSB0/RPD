5c06545cab9a96333d1a26e29a49d5f45ff0cf78
Alex Lelekov <a.t.lelekov@yandex.ru>
2020-01-03 21:42:24 +0700

    Заработал рекурсивный обход словаря из JSON. Теперь (вроде бы) сформирован словарь тегов.

--- Syllabus.py
+++ Syllabus.py
@@ -40,19 +40,17 @@ def GetJsonFromFile(filePath):
 CapFirstLetter = lambda x: x[0].upper() + x[1:]
 
 def iterData(dataJSON, keyPrefix=''):
-    """ Создаём словарь (или список) с ключами, которые соответствуют ключам в fodt
+    """ Создаём словарь с ключами, которые соответствуют ключам в fodt
+    Если есть списки, их игнорируем - они обрабатываются отдельно как таблицы
     """
-    Data = {}
     if type(dataJSON)==dict:
+        Data = {}
         for (key, value) in dataJSON.items():
-            newKey = keyPrefix+CapFirstLetter(key)
-            newValue = iterData(value, keyPrefix=newKey)
-            Data.update({newKey: newValue})
-    elif type(dataJSON)==list:
-        for item in dataJSON:
-            Data.append(iterData(value, keyPrefix=newKey))
+            Prefix = keyPrefix+CapFirstLetter(key)
+            newDict = iterData(value, keyPrefix=Prefix)
+            Data.update(newDict)
     else:
-        Data = dataJSON
+        Data = {keyPrefix:dataJSON}
     return Data
 
 """ ! как поставить в соответствие теги в документе и в json?
@@ -65,98 +63,21 @@ def iterData(dataJSON, keyPrefix=''):
 raw = GetJsonFromFile(os.path.join(folder, fileJSON))
 dataJSON = json.loads(raw)
 
-data = iterData(dataJSON)
+dTag = iterData(dataJSON)
 
 
 
 
 
 # --------------------------------------- РАБОТА С ШАБЛОНОМ
-def printChild(obj):
-    print('--- Объект "%s" содержит %d узлов:'%(obj.tag, len(obj)))
-    for child in obj:
-        print('\t%s'%(child.tag,))
-
-
 # -------- Читаем шаблон fodt
-fileIn = 'layTest.fodt'
+fileIn = 'layout.fodt'
 fileOut = 'syllabus.fodt'
 
-tree = etree.parse(os.path.join(folder, fileIn))
-document = tree.getroot()
-body = document.find('{urn:oasis:names:tc:opendocument:xmlns:office:1.0}body')
-text = body.find('{urn:oasis:names:tc:opendocument:xmlns:office:1.0}text')
+with open(os.path.join(folder, fileIn), "r") as file:
+    # read a list of lines into data
+    lines = file.readlines()
 
-printChild(text)
-
-# 1. находим первую таблицу - это подписи
-for i in range(len(text)):
-    if text[i].tag.endswith('table'):
-        break
-# 2. Переводим ее в текст и сохраняем
-PersonTable = etree.tostring(text[i], encoding="utf-8").decode('utf-8')
-
-with open(os.path.join(folder, 'PersonTable.xml'), "w") as text_file:
-    text_file.write(PersonTable)
-
-# 3. Меняем значения
-PersonTable = PersonTable.replace('Table3', 'TableAuthor')
-PersonTable = PersonTable.replace('{PersonAuthorDegree}', 'к.т.н.')
-PersonTable = PersonTable.replace('{PersonAuthorRank}', 'доцент')
-PersonTable = PersonTable.replace('{PersonAuthorPosition}', 'доцент каф. САУ')
-PersonTable = PersonTable.replace('{PersonAuthorName}', 'А.Т. Лелеков')
-
-# 4. Парсим в структру и перезаписываем одну из
-pTable = etree.fromstring(PersonTable)
-
-text.remove(text[i])
-text.insert(i, pTable)
-text.insert(5, pTable)
-printChild(text)
-
-tree.write(open(os.path.join(folder, 'layNew.fodt'), 'wb'), encoding='utf-8')
-
-
-
-
-remove(subelement)
-
-
-pColl = text.findall('{urn:oasis:names:tc:opendocument:xmlns:text:1.0}p')
-for p in pColl:
-    if not(p.text is None):
-        print(p.text)
-
-p = pColl[0]
-p.text
-p.attrib
-
-table = text.findall('{urn:oasis:names:tc:opendocument:xmlns:table:1.0}table')[0]
-
-len(table)
-printChild(table)
-
-cols = table.findall('{urn:oasis:names:tc:opendocument:xmlns:table:1.0}table-column')
-c0 = cols[0]
-
-printChild(cols)
-
-for p in tColl:
-    if not(p.text is None):
-        print(p.text)
-
-
-t.text
-
-p.text
-p.attrib
-
-
-
-
-
-
-
-
-
+for line in lines:
+    # Заменяем теги значениями
 
